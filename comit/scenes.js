fb47b7e929658e135044ee5a3bfa46a7f2e36984
srgezu <srgezu@gmail.com>
2013-04-28 19:32:38 +0200

    fd

--- comit/scenes.js
+++
@@ -1,7 +0,0 @@
-
-Crafty.scene('game', function() 
-{
-	Crafty.e('player')._set(180,145);
-	Crafty.e('manager');
-	text_score=Crafty.e("2D, DOM, Text").attr({ x: 400, y: 10,w: 100 ,h: 20 }).text(+score).css({fontFamily: 'Arial', fontSize: '12px',weight: 'bold'}).textColor('#FFFFFF');	
-});
